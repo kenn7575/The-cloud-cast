95832da826352dc8e4acb1b0b32d9b6fa1b47c21
Kenni Kollemorten <kenn7575@gmail.com>
2023-03-29 15:26:32 +0200

    update api

--- src/lib/data/apis/GeoCoding.js
+++ src/lib/data/apis/GeoCoding.js
@@ -1,16 +1,19 @@
 const Encode = async (cityName) => {
-  const response = await fetch(
-    `http://api.positionstack.com/v1/forward
-    ? access_key = 38b187306b4faf606305861143060869
-    & query = ` + cityName
-  );
-  const data = await response.json();
+  // const response = await fetch(
+  //   `http://api.positionstack.com/v1/forward
+  //   ? access_key = 38b187306b4faf606305861143060869
+  //   & query = ` + cityName
+  // );
+  // const data = await response.json();
 };
 const Decode = async (lat, lon) => {
-  const response = await fetch(
-    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=15addd394224470e81489b234bb6a147`
-  );
-  const data = await response.json();
-  return { city: data.results[0].city, country: data.results[0].country };
+  // const response = await fetch(
+  //   `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=15addd394224470e81489b234bb6a147`
+  // );
+  // const data = await response.json();
+  // return { city: data.results[0].city, country: data.results[0].country };
+  return { city: "Copenhagen", country: "Denmark" };
 };
 export { Encode, Decode };
+
+//this commented out to save api usage
