0735c0c0874bab95e7a3a66d62320dbbd85bd2d4
David <davidws13@outlook.com>
2023-03-31 09:21:29 +0200

    Added component C

---
+++ src/lib/components/DailyForecast-C.svelte
@@ -0,0 +1,23 @@
+<script>
+import { DailyWeatherData } from "../data/stores/WeatherDataStores.js";
+import DailyForecast from "./DailyForecast-D.svelte";
+
+
+$: DailyData = $DailyWeatherData;
+</script>
+
+<div class="rows">
+    {#if DailyData != null}
+        {#if DailyData.length === 0}
+        <p>Loading...</p>
+        {/if}
+        {#each DailyData as hour}
+        <DailyForecast
+            time={hour.time}
+            icon={hour.weathercode}
+            temp_max={hour.temp}
+            temp_min={hour.temp}
+        />
+        {/each}
+    {/if}
+</div>
