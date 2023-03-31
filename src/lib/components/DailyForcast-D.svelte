fb593478e4bb82da13e270fb96e2cb2884b72e9b
Kenni Kollemorten <kenn7575@gmail.com>
2023-03-31 09:19:25 +0200

    add sub component daily d

---
+++ src/lib/components/DailyForcast-D.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let time;
+  export let icon;
+  export let temp_max;
+  export let temp_min;
+</script>
+
+<div class="row">
+  <h5>{time}</h5>
+  <div class="svg">
+    {@html icon}
+  </div>
+  <div class="temperatures">
+    <p class="min">{temp_min + "°"}</p>
+    <p>{temp_max + "°"}</p>
+  </div>
+</div>
+
+<style>
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    gap: 0.5rem;
+  }
+  .svg {
+    width: 50%;
+    height: 50%;
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .temperatures {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+  }
+  .min {
+    opacity: 0.75;
+  }
+</style>
