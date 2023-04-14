cf295bafcd86650795f1f8537a79ea0903bd45e6
Kenni Kollemorten <kenn7575@gmail.com>
2023-04-14 10:54:05 +0200

    added fix to searchbox

--- src/lib/components/SearchBox.svelte
+++ src/lib/components/SearchBox.svelte
@@ -8,6 +8,9 @@
   let searchRequest = "";
   $: listOfCompletions = [];
   $: show = listOfCompletions.length > 0;
+  if (searchRequest.length === 0) {
+    listOfCompletions = [];
+  }
 
   async function handleSerchReqest() {
     if (searchRequest.length > 2) {
