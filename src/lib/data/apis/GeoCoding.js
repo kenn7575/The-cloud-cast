a4903824e9d88d5db44569b62e6bc63c29eff172
Kenni Kollemorten <kenn7575@gmail.com>
2023-04-15 20:32:35 +0200

    fix geo decoder

--- src/lib/data/apis/GeoCoding.js
+++ src/lib/data/apis/GeoCoding.js
@@ -10,13 +10,13 @@ const Decode = async (lat, lon) => {
   const response = await fetch(
     `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=ea47387ed72e4b0db5092e389d23dd0c`
   );
-  const data = await response.json();
+  const data = await response.json().catch(() => {
+    return null;
+  });
 
   const path = data.features[0].properties;
 
   return [{ city: path.city, country: path.country }];
-
-  return [{ city: "city", country: "Denmark" }];
 };
 export { Encode, Decode };
 
